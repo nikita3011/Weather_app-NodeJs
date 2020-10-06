32be38122117bded33491342b102aa557dbbd912
Nikitaghule96 <nikita.ghule@nineleaps.com>
2020-10-06 16:19:06 +0530

    updated the forecast info.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0]+'.It is currently '+body.current.temperature+' degrees out.But it feels like '+body.current.feelslike+ ' degrees out.')
+           console.log(body[0])
+            callback(undefined,
+                body.current.weather_descriptions[0]+'.It is currently '+body.current.temperature+' degrees out.But it feels like '+body.current.feelslike+ ' degrees out. The humidity is '+body.current.humidity+ '%.')
         }
     })
 }
